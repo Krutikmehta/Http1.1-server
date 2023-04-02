ed4fbc9aa818d35b79cc63805f1435d603ef294b
Krutikmeta <pkrutikmehta@gmail.com>
2023-04-02 18:02:47 +0530

    remove temp files

--- client.rb
+++
@@ -1,9 +0,0 @@
-require 'socket'
-
-s= TCPSocket.new 'localhost', 8989
-
-s.each_line do |line|
-    puts line
-end
-
-s.close
--- manyclients.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for N in {1..21}
-do
-    time python client.py
-done
-wait
